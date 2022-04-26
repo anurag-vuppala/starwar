22105c4becdfe2b815abf4bc8ee4a0d57cd09aaa
anurag-vuppala <75685045+anurag-vuppala@users.noreply.github.com>
2022-04-26 02:58:51 +0100

    final commit

--- dags/starwar_dag.py
+++ dags/starwar_dag.py
@@ -5,9 +5,24 @@ from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
 
 
-#[start_Extract_data_from_api]#
+args={
+    # 'email': ['anuragvuppala@gmail.com'],
+    'retries': 1,
+    'retry_delay': timedelta(seconds=10),
+}
 
-def starwar_api():
+with DAG(
+    dag_id="starwarapi",
+    default_args=args,
+    schedule_interval=timedelta(seconds=60),
+    start_date=datetime(2021, 1, 1),
+    catchup=False,
+    tags=["API"],
+) as dag:
+    
+    
+    #[start_Extracting_data_from_api]#
+    def starwar_api(*args):
         import requests
         import json
         
@@ -25,87 +40,63 @@ def starwar_api():
             
         else:
             print(f'Request Failed! -- Http error code {api_req.status_code}')
-#[END_Extract_data_from_api]#   
+    #[END_Extract_data_from_api]#   
 
 
-#[Start_transforming_data]#
-def store_data(*args):
-    import pandas
-    import json
-    
-    
-    f = open('request.json')
-    data = json.load(f)
-    name = []
-    model = []
-    speed = []
-    cost = []
-    pass_cap = []
-    
-    
-    # filtering responce data #
-    
-    for s in data["results"]:
-        name.append(s["name"])
-        model.append(s["model"])
-        speed.append(s["max_atmosphering_speed"])
-        cost.append(s["cost_in_credits"])
-        pass_cap.append(s["passengers"])
-    # json to distionary  #
-    ship_dic = {
-        "Spaceship_Name" : name,
-        "Spaceship_Model" : model,
-        "Max_Speed" :speed, 
-        "Spaceship_Cost" : cost,
-        "Passenger_Capacity":pass_cap,
-    }
-    # Distionary to pandas dataframe ##
-    ship_df = pandas.DataFrame(ship_dic, columns= ["Spaceship_Name",
-                                                "Spaceship_Model",
-                                                "Max_Speed",
-                                                "Spaceship_Cost",
-                                                "Passenger_Capacity",])
-    
-    # Creating .CSV file from pandas dataframe ##
-    ship_df.to_csv("/opt/airflow/output/Output_data.csv")
-    
-
-
-# data = starwar_api()
-# store_data(data)
-      
-
-
-
-
-args={
-    # 'email': ['anuragvuppala@gmail.com'],
-    'retries': 1,
-    'retry_delay': timedelta(seconds=10),
-}
-
-with DAG(
-    dag_id="starwarapi",
-    default_args= args,
-    schedule_interval=timedelta(minutes=1),
-    start_date=datetime.today(),
-    catchup=False,
-    tags=["API"],
-) as dag:
+    #[Start_transforming_data]#
+    def store_data(*args):
+        import pandas
+        import json
         
         
-        extract = PythonOperator(task_id = "extract",
-                                    python_callable=starwar_api)
-            
-        transform_load = PythonOperator(task_id = "transform_load",
-                                            python_callable = store_data) 
-            
-        extracted = BashOperator(task_id = "extracted",
-                                    bash_command = "echo 'Data_Extracted'")
-            
-        loaded = BashOperator(task_id = "loaded",
-                                bash_command = "echo 'Data_loaded_to_CSV'"
-        )
+        f = open('request.json')
+        data = json.load(f)
+        name = []
+        model = []
+        speed = []
+        cost = []
+        pass_cap = []
         
         
-        extract >> extracted >> transform_load >> loaded
+        # filtering responce data #
+        
+        for s in data["results"]:
+            name.append(s["name"])
+            model.append(s["model"])
+            speed.append(s["max_atmosphering_speed"])
+            cost.append(s["cost_in_credits"])
+            pass_cap.append(s["passengers"])
+        # json to distionary  #
+        ship_dic = {
+            "Spaceship_Name" : name,
+            "Spaceship_Model" : model,
+            "Max_Speed" :speed, 
+            "Spaceship_Cost" : cost,
+            "Passenger_Capacity":pass_cap,
+        }
+        # Distionary to pandas dataframe ##
+        ship_df = pandas.DataFrame(ship_dic, columns= ["Spaceship_Name",
+                                                    "Spaceship_Model",
+                                                    "Max_Speed",
+                                                    "Spaceship_Cost",
+                                                    "Passenger_Capacity",])
+        
+        # Creating .CSV file from pandas dataframe ##
+        ship_df.to_csv("/opt/airflow/output/Output_data.csv")
+        
+        
+    extract = PythonOperator(task_id = "extract",
+                                python_callable=starwar_api)
+        
+    transform_load = PythonOperator(task_id = "transform_load",
+                                        python_callable = store_data) 
+        
+    extracted = BashOperator(task_id = "extracted",
+                                bash_command = "echo 'Data_Extracted'")
+        
+    loaded = BashOperator(task_id = "loaded",
+                            bash_command = "echo 'Data_loaded_to_CSV'"
+    )
+    
+    
+    extract >> extracted >> transform_load >> loaded
